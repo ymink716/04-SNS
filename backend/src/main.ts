f1c6d33c2746868e415fdf6bc096af3a80dff625
LeoKim <leo3179@naver.com>
2022-07-20 18:07:53 +0900

    ADD / http exception,validation pipe 설치
    - http 예외처리 filter 추가
    - main.ts에 예외처리 filter와 validation pipe를 global로 적용
    - nestjs timezone KR로 설정

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentationOptions } from './config/swagger/documentationOptions';
+import { DocumentationOptions } from './common/config/swagger/documentationOptions';
 import { SwaggerModule } from '@nestjs/swagger';
+import { HttpExceptionFilter } from './common/filter/httpExceptionFilter';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      enableDebugMessages: true,
+      exceptionFactory(errors) {
+        const message = Object.values(errors[0].constraints);
+        throw new BadRequestException(message[0]);
+      },
+    }),
+  );
 
   const documentOptions = new DocumentationOptions().initializeOptions();
   const document = SwaggerModule.createDocument(app, documentOptions);
