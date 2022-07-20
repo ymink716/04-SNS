c3389b891d4855a0fd5ce118005cc78072f9cadb
LeoKim <leo3179@naver.com>
2022-07-20 15:12:50 +0900

    ADD / TypeORM 설정
    - TypeORM 모듈 설정 (env 처리)
    - env 파일, 폴더 생성 및 .gitignore에 추가
    - MySQL, TypeORM 관련 패키지 설치

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './apis/app/app.controller';
 import { AppService } from './apis/app/app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/apis/**/*.entity.*'],
+      synchronize: true,
+      logging: true,
+      retryAttempts: 30,
+      retryDelay: 5000,
+      connectTimeout: 15000,
+      timezone: 'Z',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
